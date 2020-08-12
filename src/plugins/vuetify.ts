a763648be3e451345ab692e8d11389fd4f765bd2
Nate Stringham <nstringham@wccnet.edu>
2020-08-11 23:02:43 -0400

    changed main theme color

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,4 +3,16 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+    theme: {
+        dark: true,
+        themes: {
+            light: {
+                primary: '#c20312',
+            },
+            dark: {
+                primary: '#c20312',
+            },
+        },
+    },
+});
