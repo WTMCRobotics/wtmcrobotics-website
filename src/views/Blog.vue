900a0aefef54198fb0cec9a5cf6bfc4cd6f1d5ea
Nate Stringham <nstringham@wccnet.edu>
2020-08-21 02:39:24 -0400

    made blogs load a few at a time

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,12 +1,15 @@
 <template>
-  <v-container fluid class="grid">
+  <v-container fluid class="grid" ref="container">
     <PostCard
       v-for="post in posts"
-      :key="post.data.title"
+      :key="post.id"
       :to="`blog/${post.id}`"
-      :post="post.data"
+      :post="post.data()"
       :card="true"
     ></PostCard>
+    <div v-if="!loadedAll" class="load-more">
+      <v-btn text :loading="loading" @click="loadMore">Load more</v-btn>
+    </div>
   </v-container>
 </template>
 
@@ -16,35 +19,65 @@
   gap: 16px;
   grid-template-columns: repeat(auto-fit, minmax(min(300px, 90%), 1fr));
 }
+.load-more {
+  grid-column: 1 / -1;
+  justify-self: center;
+}
 </style>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import { firestore, BlogPost } from "../firebase";
 import PostCard from "../components/PostCard.vue";
+
 @Component({
   components: { PostCard }
 })
 export default class Blog extends Vue {
-  posts: { data: BlogPost; id: string }[] = [];
+  posts: firebase.firestore.QueryDocumentSnapshot<BlogPost>[] = [];
+  loadedAll = false;
+  loading = false;
 
-  beforeCreate() {
-    firestore
-      .collection("blogs")
-      .where("public", "==", true)
-      .orderBy("date", "desc")
-      .limit(100) // TODO make a propper system
+  mounted() {
+    this.loadMore();
+  }
+
+  loadMore() {
+    if (this.loadedAll || this.loading) {
+      return;
+    }
+    this.loading = true;
+    let collection = (firestore.collection(
+      "blogs"
+    ) as firebase.firestore.CollectionReference<BlogPost>).orderBy(
+      "date",
+      "desc"
+    );
+    // eslint-disable-next-line no-constant-condition
+    if (true) {
+      // TODO make this if not an editor
+      collection = collection.where("public", "==", true);
+    }
+    const length = this.posts.length;
+    if (length > 0) {
+      collection = collection.startAfter(this.posts[length - 1]);
+    }
+    const limit = this.makeEven(2 + length) - length; // TODO make this not 2
+    collection
+      .limit(limit)
       .get()
       .then(snapshot => {
-        snapshot.docs.forEach((doc, i) => {
-          if (doc.exists) {
-            this.$set(this.posts, i, {
-              data: doc.data() as BlogPost,
-              id: doc.id
-            });
-          }
-        });
+        this.loadedAll = limit !== snapshot.docs.length;
+        this.posts = this.posts.concat(snapshot.docs);
+        this.loading = false;
       });
+  }
+
+  makeEven(input: number) {
+    const width = Math.floor(
+      ((this.$refs["container"] as Element).clientWidth - 8) / 316
+    );
+    return Math.ceil(input / width) * width;
   }
 }
 </script>
