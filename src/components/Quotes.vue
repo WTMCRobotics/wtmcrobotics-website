af6c141b9a2ce08901c48fe9767122a3ae8102c0
Nate Stringham <nstringham@wccnet.edu>
2020-11-25 17:16:58 -0500

    refactor: move quotes to it's own component

---
+++ src/components/Quotes.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-container fluid>
+    <h2>Our students are saying</h2>
+    <div>
+      <figure>
+      <blockquote>
+        <p>
+          “It's fun getting to do small jobs with different machines and know
+          that I can do that and that I was able to help build something.”
+        </p>
+      </blockquote>
+      <figcaption>—Morgan Salzano</figcaption>
+    </figure>
+    <figure>
+      <blockquote>
+        <p>
+          “I have learned how to program and wire a robot, and more importantly
+          have had the wonderful experience of building said awesome robot with
+          a bunch of awesome people and then see said awesome robot succeed.”
+        </p>
+      </blockquote>
+      <figcaption>—Miranda McCarthy</figcaption>
+    </figure>
+    <figure>
+      <blockquote>
+        <p>
+          "FIRST has impacted me because it has allowed me to put some of my
+          skills into - practice. For example, I want to go into business, and
+          currently I am taking an Excel spreadsheet class. With FIRST, I am
+          able to use those same spreadsheet skills to make the budget for the
+          Robotics team”
+        </p>
+      </blockquote>
+      <figcaption>—Adrian Fazecas</figcaption>
+    </figure>
+    <figure>
+      <blockquote>
+        <p>
+          “It's fun getting to do small jobs with different machines and know
+          that I can do that and that I was able to help build something.”
+        </p>
+      </blockquote>
+      <figcaption>—Morgan Salzano</figcaption>
+    </figure>
+    </div>
+  </v-container>
+</template>
+
+<style lang="scss" scoped>
+</style>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { namespace } from "vuex-class";
+import { Quote } from "@/firebase";
+
+const quoteModule = namespace("quotes");
+
+@Component
+export default class Quotes extends Vue {
+  @quoteModule.State loading!: boolean;
+  @quoteModule.State quotes!: Quote[];
+
+  constructor() {
+    super();
+    this.load();
+  }
+
+  @quoteModule.Action load!: () => void;
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -66,47 +66,7 @@
     <label>Mechanical <meter min="0" max="100" value="50"></meter></label><br />
     <label>Business <meter min="0" max="100" value="25"></meter></label><br />
     <label>Programming <meter min="0" max="100" value="25"></meter></label>
-    <h1>Our students are saying</h1>
-    <figure>
-      <blockquote>
-        <p>
-          “It's fun getting to do small jobs with different machines and know
-          that I can do that and that I was able to help build something.”
-        </p>
-      </blockquote>
-      <figcaption>—Morgan Salzano</figcaption>
-    </figure>
-    <figure>
-      <blockquote>
-        <p>
-          “I have learned how to program and wire a robot, and more importantly
-          have had the wonderful experience of building said awesome robot with
-          a bunch of awesome people and then see said awesome robot succeed.”
-        </p>
-      </blockquote>
-      <figcaption>—Miranda McCarthy</figcaption>
-    </figure>
-    <figure>
-      <blockquote>
-        <p>
-          "FIRST has impacted me because it has allowed me to put some of my
-          skills into - practice. For example, I want to go into business, and
-          currently I am taking an Excel spreadsheet class. With FIRST, I am
-          able to use those same spreadsheet skills to make the budget for the
-          Robotics team”
-        </p>
-      </blockquote>
-      <figcaption>—Adrian Fazecas</figcaption>
-    </figure>
-    <figure>
-      <blockquote>
-        <p>
-          “It's fun getting to do small jobs with different machines and know
-          that I can do that and that I was able to help build something.”
-        </p>
-      </blockquote>
-      <figcaption>—Morgan Salzano</figcaption>
-    </figure>
+    <Quotes />
     <Sponsors />
   </div>
 </template>
@@ -115,11 +75,13 @@
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue";
 import Sponsors from "@/components/Sponsors.vue";
+import Quotes from "@/components/Quotes.vue";
 
 @Component({
   components: {
     HelloWorld,
     Sponsors,
+    Quotes
   },
   metaInfo: { titleTemplate: "" },
 })
