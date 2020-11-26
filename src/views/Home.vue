c1f30eeb84c8eac270f059038beb83df6e29f8c1
Nate Stringham <nstringham@wccnet.edu>
2020-11-25 19:46:28 -0500

    style: remove whitespace

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,9 +52,9 @@
       past, we’ve hosted a summer camp to educate children in STEM fields and
       volunteered at places like Food Gatherers, Maker Faire, and the Ann Arbor
       Summer Festival
-    </p>  
+    </p>
     <h2>Our students are saying</h2>
-    <Quotes />    
+    <Quotes />
     <h2>Our sponsors</h2>
     <Sponsors />
   </div>
