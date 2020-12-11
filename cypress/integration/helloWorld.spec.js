b361af5a5503efed16a2fd98e06d4ba94073863e
Nate Stringham <nstringham@wccnet.edu>
2020-12-11 06:25:06 -0500

    CI: test actual webpage now

--- cypress/integration/helloWorld.spec.js
+++ cypress/integration/helloWorld.spec.js
@@ -4,4 +4,8 @@ describe('Hello World', () => {
     it('test if cypress is working', () => {
         expect(0).to.equal(0)
     })
+
+    it('makes sure the page exists', () => {
+        cy.visit('http://localhost:5000')
+    })
 })
