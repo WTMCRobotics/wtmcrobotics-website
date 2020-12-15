c707cb406d6931d185212663d5e23f2d5b1a8a93
Nate Stringham <nstringham@wccnet.edu>
2020-12-15 00:29:07 -0500

    feat(CI): send lighthouse to more urls

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,14 @@
 module.exports = {
     ci: {
         collect: {
-            url: ['http://localhost:5000/'],
+            url: [
+                'http://localhost:5000/',
+                'http://localhost:5000/gallery',
+                'http://localhost:5000/sponsor',
+                'http://localhost:5000/join',
+                'http://localhost:5000/blog',
+                'http://localhost:5000/contact'
+            ],
             startServerCommand: 'firebase emulators:start',
         },
         upload: {
