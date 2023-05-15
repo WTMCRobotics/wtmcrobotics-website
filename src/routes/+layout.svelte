3daa65024718727b8acaa9af6831892f96f418b3
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2023-05-15 18:34:37 -0400

    feat: ✨ add nav and pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,31 @@
-<script>
+<script lang="ts">
   import "../app.postcss";
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
+  import { page } from "$app/stores";
+
+  $: route = $page.route.id;
+
+  const navLinks = [
+    { url: "/", label: "Home" },
+    { url: "/gallery", label: "Gallery" },
+    { url: "/sponsor", label: "Sponsor" },
+    { url: "/join", label: "Join" },
+    { url: "/blog", label: "Blog" },
+    { url: "/contact", label: "Contact" },
+  ];
 </script>
+
+<Navbar let:hidden let:toggle class="sticky w-full z-20 top-0 left-0 border-b">
+  <NavBrand href="/">
+    <img src="http://wtmcrobotics.com/style/images/black_logo_500x500.png" class="mr-3 h-12" alt="WTMC Robotics Logo" />
+    <span class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">WTMC Robotics</span>
+  </NavBrand>
+  <NavHamburger on:click={toggle} />
+  <NavUl {hidden}>
+    {#each navLinks as { url, label }}
+      <NavLi href={url} active={url == route}>{label}</NavLi>
+    {/each}
+  </NavUl>
+</Navbar>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { Heading, P, A } from "flowbite-svelte";
+</script>
+
+<div class="px-8">
+  <Heading tag="h1" class="my-4">Welcome to SvelteKit</Heading>
+  <P>Visit <A href="https://kit.svelte.dev">kit.svelte.dev</A> to read the documentation</P>
+</div>
