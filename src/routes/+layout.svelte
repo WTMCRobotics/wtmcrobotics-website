c7e513ffd6f9359ea4fbafa757107c4308cf7b48
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2023-05-15 19:44:17 -0400

    feat: ✨ dark mode and Robotics Red

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,13 +15,17 @@
   ];
 </script>
 
-<Navbar let:hidden let:toggle class="sticky w-full z-20 top-0 left-0 border-b">
+<Navbar let:hidden let:toggle class="sticky w-full z-20 top-0 left-0 border-b dark:bg-gray-950 dark:border-gray-800">
   <NavBrand href="/">
     <img src="http://wtmcrobotics.com/style/images/black_logo_500x500.png" class="mr-3 h-12" alt="WTMC Robotics Logo" />
     <span class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">WTMC Robotics</span>
   </NavBrand>
   <NavHamburger on:click={toggle} />
-  <NavUl {hidden}>
+  <NavUl
+    {hidden}
+    activeClass="text-white bg-primary-900 md:bg-transparent md:text-primary-900 md:dark:text-white dark:bg-primary-900 md:dark:bg-transparent"
+    nonActiveClass="text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-900 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+  >
     {#each navLinks as { url, label }}
       <NavLi href={url} active={url == route}>{label}</NavLi>
     {/each}
